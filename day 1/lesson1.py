018381466d3d420b60c2fc8ceff99a20522c21ad
lukakhotcholava <khotcholavaluka@gmail.com>
2023-05-25 21:29:55 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,30 @@
+word0 = "salami,"
+
+word = "me " " var "
+
+name = "luka"
+
+surname = "khotcholava,"
+
+height = "169cm"
+
+word1 = "simaghlis,"
+
+word2 = "wonashi"
+
+weight = "68kg,"
+
+word3 = "profesiit " " var "
+
+profession = "barista-administratori,"
+
+word4 = " ukve titkmis 1 celia,"
+
+word5 = "minda"
+
+purpose_of_life = "viko carmatebuli kvelgan, kvela sferoshi."
+
+word6 = "sul es iko, momaval shexvedramde."
+
+
+print(word0 + " " + word + name + " " + surname + " " + height + " " + word1 + " " + word2 + " " + weight + " " + word3 + " " + profession + " " + word4 + " " + word5 + " " + purpose_of_life + " " + word6)
